5e218d301937e075cc659e1299dc4eec7159565e
ZiyanN <ziyannadeem@outlook.com>
2026-04-29 01:12:17 +1000

    Content View

--- MalClone/ContentView.swift
+++ MalClone/ContentView.swift
@@ -7,32 +7,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var title = "Loading..."
-
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(title)
-        }
-        .padding()
-        .task {
-            do {
-                print("🔵 fetching anime id 1...")
-                let json = try await APIService.shared.fetchAnimeFull(id: 31
-)
-                print("🟢 raw response: \(json)")
-                
-                let data = json["data"] as? [String: Any]
-                print("🟢 data block: \(String(describing: data))")
-                
-                title = data?["title"] as? String ?? "no title"
-                print("🟢 title: \(title)")
-            } catch {
-                print("🔴 error: \(error)")
-                title = error.localizedDescription
-            }
+    var body: some View{
+        VStack{
+            Text("E")
         }
     }
 }

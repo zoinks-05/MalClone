8fb4b595f577f364832281585755b8974a645b8c
ZiyanN <ziyannadeem@outlook.com>
2026-04-28 13:45:09 +1000

    Initial Commit

---
+++ MalClone/MalCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalCloneApp.swift
+//  MalClone
+//
+//  Created by Ziyan Nadeem on 28/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct MalCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

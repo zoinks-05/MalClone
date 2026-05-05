b04fdb4f5bb53db07d7ccdb99a27bbc5791467c3
BrianTranGit <xxbrianplayz@gmail.com>
2026-05-05 13:54:23 +1000

    Created profile view

--- MalClone/Profile/Profile.swift
+++ MalClone/Profile/Profile.swift
@@ -5,3 +5,17 @@
 //  Created by Ziyan Nadeem on 29/4/2026.
 //
 
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Works")
+        // Fake Profile Image
+        // User Model
+        // Bio
+        // Picker to swap besides watchlist and reviews
+        // Watchlist should be able to remove/edit using the addWatchList view as reference and structure in extension_animedetails
+        // Review should be able to edit reviews using the reviewPublishView() as reference and structure in AnimeDetails
+        // Use sheets and showreviewsheet = false
+    }
+}
